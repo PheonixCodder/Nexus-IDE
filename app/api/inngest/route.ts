85bb45f0e707476050453a2af6f0becfe5f478de
Ubaidullah Ismail <abdulahadismail.dev@gmail.com>
2026-03-13 10:33:03 -0700

    Setup Conversations

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,7 +1,10 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
+import { importGithubRepo } from "@/features/projects/inngest/import-github-repo";
+import { exportToGithub } from "@/features/projects/inngest/export-to-github";
+import { processMessage } from "@/features/conversations/inngest/process-message";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [],
+  functions: [processMessage, importGithubRepo, exportToGithub],
 });
